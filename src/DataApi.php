d007ed13ecfb7fa7ef000f3419d44725d021b5aa
emil <emil@htmlbox.net>
2024-09-07 10:27:09 +0300

    Improve data API layer

--- src/DataApi.php
+++ src/DataApi.php
@@ -16,8 +16,35 @@ class DataApi {
 		$this->table_name = $wpdb->prefix . 'chb_trades';
 	}
 
-	function get_trades(int $page = 1, int $per_page = 30) {
-		$start = ($page - 1) * $per_page;
-		return $this->wpdb->get_results("SELECT * FROM $this->table_name order by id DESC LIMIT $start, $per_page");
+	function get_trades(
+		array $filter = [],
+		int $page = 1,
+		int $per_page = 30,
+	) {
+		$start_row = ($page - 1) * $per_page;
+		$allowed_columns = [
+			'representative', 'bio_guide_id', 'report_date',
+			'transaction_date', 'ticker', 'transaction',
+			'range_min', 'range_max', 'house',
+			'amount', 'party',
+		];
+
+		$filter_sql_pieces = ['1 = 1'];
+		foreach ($filter as $column => $search_value) {
+			if (!in_array($column, $allowed_columns)) {
+				throw new \RuntimeException("Unknown columng $column");
+			}
+
+			$filter_sql_pieces[] = '`' . $column . '`="' . esc_sql($search_value) . '"';
+		}
+
+		return $this->wpdb->get_results("
+			SELECT *
+			FROM $this->table_name
+			WHERE " . implode( ' AND ', $filter_sql_pieces ) . "
+			ORDER BY transaction_date DESC
+			LIMIT $start_row, $per_page
+		");
 	}
+
 }
